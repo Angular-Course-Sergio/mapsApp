b365b8206b4394fdce8eeff706b39b3564709869
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-11 15:13:43 -0700

    Mostrar un mapa en pantalla completa

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import {Map} from 'mapbox-gl';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrl: './full-screen-page.component.css',
 })
-export class FullScreenPageComponent {
-
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new Map({
+      accessToken: environment.mapbox_key,
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }
 }
