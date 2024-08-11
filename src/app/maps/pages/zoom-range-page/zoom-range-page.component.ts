209486523e8e1fc1633821358e5eea84f80efbe2
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-11 16:25:48 -0700

    Controlar el nivel del Zoom

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -10,15 +10,46 @@ import { environment } from '../../../../environments/environments';
 export class ZoomRangePageComponent implements AfterViewInit {
   @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       accessToken: environment.mapbox_key,
       container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-74.5, 40],
-      zoom: 9,
+      zoom: this.zoom,
     });
+
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 }
