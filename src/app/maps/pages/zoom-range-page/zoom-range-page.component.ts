634d73cb7127e310c3c542f54014598ac7fda233
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-11 15:42:53 -0700

    Diseño - ZoomIn y ZoomOut

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Map } from 'mapbox-gl';
+
+import { environment } from '../../../../environments/environments';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.css'
+  styleUrl: './zoom-range-page.component.css',
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
 
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      accessToken: environment.mapbox_key,
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }
 }
