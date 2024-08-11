5e85c687aeefa91aa7dc24f676dcbcbd47876031
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-11 15:28:58 -0700

    ViewChild - ElementRef

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
-import {Map} from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 import { environment } from '../../../../environments/environments';
 
 @Component({
@@ -7,10 +7,14 @@ import { environment } from '../../../../environments/environments';
   styleUrl: './full-screen-page.component.css',
 })
 export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+
   ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
     const map = new Map({
       accessToken: environment.mapbox_key,
-      container: 'map',
+      container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [-74.5, 40],
       zoom: 9,
