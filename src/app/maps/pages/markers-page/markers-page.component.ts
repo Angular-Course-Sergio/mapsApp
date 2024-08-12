c33528399e82bd2d9a1bc4facd2a1257302af9c7
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-11 17:15:07 -0700

    Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
+import { environment } from '../../../../environments/environments';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrl: './markers-page.component.css'
+  styleUrl: './markers-page.component.css',
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
 
+  // public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -109.95176143233326,
+    27.50671078875679
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      accessToken: environment.mapbox_key,
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.currentLngLat,
+      zoom: 13,
+    });
+  }
 }
