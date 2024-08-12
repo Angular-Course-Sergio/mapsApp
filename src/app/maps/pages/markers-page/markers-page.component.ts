7d58109e8fa97aa9cbf2bd84313d6700dbf3d514
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-11 17:21:50 -0700

    Añadir marcadores de forma dinámica

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -27,4 +27,25 @@ export class MarkersPageComponent implements AfterViewInit {
       zoom: 13,
     });
   }
+
+  createMarker() {
+    if (!this.map) return;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
+
+    const marker = new Marker({
+      color,
+      draggable: true,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+  }
 }
