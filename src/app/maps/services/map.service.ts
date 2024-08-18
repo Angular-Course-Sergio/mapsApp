9a9fded42bc469d05bb8e8516f785e172d84f515
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-18 16:25:11 -0700

    Ajustar el mapa para mostrar todos los marcadores encontrados

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interface';
 
 @Injectable({
@@ -26,7 +26,7 @@ export class MapService {
     });
   }
 
-  createMarkersFromPlaces(places: Feature[]) {
+  createMarkersFromPlaces(places: Feature[], userLocation: [number, number]) {
     if (!this.map) throw Error('Mpa no inicializado');
 
     this.markers.forEach((marker) => marker.remove());
@@ -51,5 +51,15 @@ export class MapService {
     }
 
     this.markers = newMarkers;
+    if (places.length === 0) return;
+
+    // Map limits
+    const bounds = new LngLatBounds();
+    this.markers.forEach((marker) => bounds.extend(marker.getLngLat()));
+    bounds.extend(userLocation);
+
+    this.map.fitBounds(bounds, {
+      padding: 200,
+    });
   }
 }
