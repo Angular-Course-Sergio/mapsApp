99bbc2695fec4ca14ec897b84023d993d2dab5ab
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-18 17:13:55 -0700

    Dibujar la polyline

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,5 +1,12 @@
 import { inject, Injectable } from '@angular/core';
-import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import {
+  LngLatBounds,
+  LngLatLike,
+  Map,
+  Marker,
+  Popup,
+  SourceSpecification,
+} from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interface';
 import { DirectionsApliClient } from '../api/directionsApiClient';
 import { DirectionsResponse, Route } from '../interfaces/directions.interface';
@@ -84,6 +91,44 @@ export class MapService {
 
     this.map?.fitBounds(bounds, {
       padding: 200,
+    });
+
+    const sourceData: SourceSpecification = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords,
+            },
+          },
+        ],
+      },
+    };
+
+    if (this.map.getLayer('RouteString')) {
+      this.map.removeLayer('RouteString');
+      this.map.removeSource('RouteString');
+    }
+
+    this.map.addSource('RouteString', sourceData);
+
+    this.map.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round',
+      },
+      paint: {
+        'line-color': 'blue',
+        'line-width': 3,
+      },
     });
   }
 }
